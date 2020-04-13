d6144bc9a628558e5fd6483ce28121371894c044
valerian.rey@gmail.com <valerian.rey@gmail.com>
2020-04-13 16:12:58 +0200

    added logs

--- Proj1/print_util.py
+++ Proj1/print_util.py
@@ -21,3 +21,20 @@ def print_test(b, batch_length, batch_time, loss, top1, persistent=True, color='
           '{top1.val:.3f} ({top1.avg:.3f})'.format(top1=top1).ljust(22) + Color.END,
           end='\n' if persistent else '')
 
+
+def log_acc_loss_header(color=''):
+    print(color + 'Epoch'.ljust(12) + 'Time'.ljust(8) + 'Train loss'.ljust(15) +
+          'Train accuracy'.ljust(20) + 'Test loss'.ljust(15) + 'Test accuracy'.ljust(20) + Color.END)
+
+
+def log_acc_loss(e, nb_epoch, time, train_loss, train_acc, test_loss, test_acc, color='', persistent=True):
+    print('\r' + color +
+          '[{0}/{1}]'.format(e + 1, nb_epoch).ljust(12) +
+          '{time:.0f}s'.format(time=time).ljust(8) +
+          '{0:.4f}'.format(train_loss).ljust(15) +
+          '{0:.4f}'.format(train_acc).ljust(20) +
+          '{0:.4f}'.format(test_loss).ljust(15) +
+          '{0:.4f}'.format(test_acc).ljust(20) +
+          Color.END,
+          end='\n' if persistent else '')
+
